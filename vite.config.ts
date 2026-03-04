89b7a86abf92056808a19fabb138c3959605b82d
milbark90-web <milbark90@gmail.com>
2026-03-04 16:53:15 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+// __dirname을 안전하게 정의합니다
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
+  // 깃허브 저장소 이름
+  base: '/mill/', 
+  
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
